cc54ae6850306a62d513f2c02f1e71e886063463
Maeci <contact@maecefischer.ch>
2019-11-10 12:06:55 +0100

    prepared webpack dev server files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,12 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
-    mode: "development"
+    mode: "development",
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        compress: true,
+        port: 9000
+    }
 });
