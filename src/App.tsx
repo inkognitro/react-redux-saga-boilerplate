5ea2dc8bd9652093c4deb130cb6d86ae373252c8
Maeci <contact@maecefischer.ch>
2019-11-24 22:39:31 +0100

    changed todos

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {Provider} from 'react-redux';
 import store from 'App/Redux/store';
 import Home from "App/Components/RouteViews/Home/Home";
 
-//todo: install @types/uuid, @types/redux-thunk, uninstall prop-types after js was migrated to ts
-//todo: remove allowJs in tsconfig.json
+//todo: install @types/uuid, @types/redux-thunk
+//todo: remove allowJs in tsconfig.json after typescript migration has been done
 
 export default class App extends React.Component {
     render() {
