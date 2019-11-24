a47be6d6cea10a1f2d8dbe0d6823712b2f34d5a6
Maeci <contact@maecefischer.ch>
2019-11-24 17:07:30 +0100

    js to ts migration, todo and libraries

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {Provider} from 'react-redux';
 import store from 'App/Redux/store';
 import Home from "App/Components/RouteViews/Home/Home";
 
+//todo : install @types/uuid, @types/redux-thunk, uninstall prop-types after js to ts integration
+
 export default class App extends React.Component {
     render() {
         return (
