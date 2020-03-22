0777021835f07d39faab4bfd902a2b916e63c8d4
Maeci <contact@maecefischer.ch>
2020-03-22 16:33:06 +0100

    removed redux thunk

--- src/Common/types.ts
+++
@@ -1,5 +0,0 @@
-import {ThunkAction, ThunkDispatch} from "redux-thunk";
-import {Action} from "redux";
-
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, object, null, Action<string>>; //todo: remove type and library!
-export type AppDispatch = ThunkDispatch<object, null, Action<string>>;
