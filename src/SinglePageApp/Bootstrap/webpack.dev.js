1d2398dc0c7336aeb8d4417417f72fbe68c5d5a7
Maeci <contact@maecefischer.ch>
2020-04-12 19:26:23 +0200

    webpack configuration changes

--- src/SinglePageApp/Bootstrap/webpack.dev.js
+++ src/SinglePageApp/Bootstrap/webpack.dev.js
@@ -5,7 +5,7 @@ const commonConfig = require('./webpack.common');
 module.exports = merge(commonConfig, {
     mode: "development",
     devtool: 'inline-source-map',
-    entry: path.resolve(__dirname, './entry.dev.js'),
+    entry:  path.resolve(__dirname, './entry.dev.js'),
     devServer: {
         historyApiFallback: true,
         contentBase: './dist/SinglePageApp',
