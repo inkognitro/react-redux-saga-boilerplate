95f0a3a6cc4a39d1ba9a10a8927f5e14f39d0988
Maeci <contact@maecefischer.ch>
2020-05-30 22:56:19 +0200

    type warning eliminiations

--- src/Packages/Common/HttpApiV1/index.ts
+++ src/Packages/Common/HttpApiV1/index.ts
@@ -1,6 +1,16 @@
-export { HttpApiV1EventTypes, ApiV1ReadResponse } from './Domain/Types';
+import { ApiV1HttpConnectionFailed as ApiV1HttpConnectionFailedType } from './Domain/Event/ApiV1HttpConnectionFailed';
+import { ApiV1HttpResponseWasReceived as ApiV1HttpResponseWasReceivedType } from './Domain/Event/ApiV1HttpResponseWasReceived';
+import { ApiV1ReadResponse as ApiV1ReadResponseType } from './Domain/Types';
+import { AuthenticateResult as AuthenticateResultType } from './Domain/Saga/CustomEffect/Authenticate';
+import { SendHttpRequest as SendHttpRequestType } from './Domain/Command/SendHttpRequest';
+
+export type AuthenticateResult = AuthenticateResultType;
+export type ApiV1HttpConnectionFailed = ApiV1HttpConnectionFailedType;
+export type ApiV1HttpResponseWasReceived = ApiV1HttpResponseWasReceivedType;
+export type ApiV1ReadResponse = ApiV1ReadResponseType;
+export type SendHttpRequest = SendHttpRequestType;
+
+export { HttpApiV1EventTypes } from './Domain/Types';
 export { createHttpApiV1Saga } from './Domain/Saga/Flow';
-export { createSendHttpRequest, SendHttpRequest } from './Domain/Command/SendHttpRequest';
-export { ApiV1HttpConnectionFailed } from './Domain/Event/ApiV1HttpConnectionFailed';
-export { ApiV1HttpResponseWasReceived } from './Domain/Event/ApiV1HttpResponseWasReceived';
-export { authenticate, AuthenticateResult } from './Domain/Saga/CustomEffect/Authenticate';
+export { createSendHttpRequest } from './Domain/Command/SendHttpRequest';
+export { authenticate } from './Domain/Saga/CustomEffect/Authenticate';
