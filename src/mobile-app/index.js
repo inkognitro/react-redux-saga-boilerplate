f0abf31134e56342dbea96865e1b91108644a9be
Maeci <contact@maecefischer.ch>
2020-08-31 16:17:40 +0200

    toaster test fix

--- src/mobile-app/index.js
+++ src/mobile-app/index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import { App } from 'mobile-app/app';
+import { App } from 'mobile-app/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/packages/src/common/toaster/ui/native/toaster.unit.test.tsx
+++ src/packages/src/common/toaster/ui/native/toaster.unit.test.tsx
@@ -1,7 +1,7 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Toaster } from "./ToasterNC";
+import { Toaster } from "./toaster";
 
 it('renders correctly', () => {
     renderer.create(<Toaster toasts={[]} onRemoveMessage={() => {}} />);
