9efe6ff644e925a9ab5c96c9f90c608281a92029
Maeci <contact@maecefischer.ch>
2020-10-11 14:08:37 +0200

    add pseudo test

---
+++ src/web-app/src/pages/home/ui/page.unit.test.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import renderer from 'react-test-renderer';
+
+const Foo: FC = () => (<>Foo</>);
+
+describe("Foo", () => {
+    it('renders correctly', () => {
+        renderer.create(<Foo />);
+    });
+});
